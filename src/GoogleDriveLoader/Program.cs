be56e3994f85743ba17feac04c36b15e24aca564
Daniel Belz <daniel_belz@gmx.net>
2019-10-29 20:05:25 +0100

    Add project files.

---
+++ src/GoogleDriveLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleDriveLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
